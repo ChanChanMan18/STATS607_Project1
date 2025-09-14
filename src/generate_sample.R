9789a8f5db4fbdf513ffc76bcdf448b3be7b991a
Chandler Nielsen <chandle@l-m2c5p2j73j.local>
2025-09-14 18:04:36 -0400

    separate sample generation from body of main Rmd file

--- src/generate_sample.R
+++ src/generate_sample.R
@@ -1,47 +1,54 @@
-
+#' Generate data according to main example of 
+#' Moscovich and Rosset (2022)
+#' 
+#' Input:
+#' n - number of samples
+#' p - number of predictors
+#' M - first M predictors scaled by constant C > 0
+#' C - positive constant
+#' distribution - select from gaussian or t-distribution (the two distributions)
+#'                considered in the paper
+#' df - degrees of freedom if distribution = "t"
+#' eta - noise level
+#' 
+#' Output: 
+#' list containing design matrix X, responses y, true coefficients beta_true,
+#' and noise variance sigma2
 generate_sample <- function(n = 200,
                             p = 100,
                             M = 5,
                             C = 5,
                             distribution = c("gaussian", "t"),
                             df = 4,
-                            eta = 1,
-                            seed) {
+                            eta = 1) {
   
   distribution <- match.arg(distribution)
   
-  ### Generating design X from chosen distribution
   if (distribution == "gaussian") {
-    
     X <- matrix(rnorm(n * p), nrow = n, ncol = p)
-    
   } else {
-    
-    ### t-distribution with 'df' degrees of freedom if not Gaussian
     X <- matrix(rt(n * p, df = df), nrow = n, ncol = p)
-    
   }
   
-  ### Scale the appropriate columns by C
+  # Scaling first M predictors for larger variance
   if (M > 0) {
     X[, 1:M] <- C * X[, 1:M]
   }
   
-  ### Generating betas from normal distribution
+  ### Generate true betas from standard normal
   beta_true <- rnorm(p, mean = 0, sd = 1)
   
   ### Generating the error terms 
   sigma2 <- eta * ((p - M) + (C^2) * M) # Noise variance
-  eps <- rnorm(n, mean = 0, sd = sqrt(sigma2)) # The error
+  eps <- rnorm(n, mean = 0, sd = sqrt(sigma2)) 
   
   ### Generating the output y
   y <- as.numeric(X %*% beta_true + eps)
   
-  ### Compressing output
-  return_list <- list(X = X, y = y, 
-                      betaTrue = beta_true, 
-                      sigma2 = sigma2)
+  output <- list(X = X, y = y, 
+                 betaTrue = beta_true, 
+                 sigma2 = sigma2)
   
-  return(return_list)
+  return(output)
   
 }
